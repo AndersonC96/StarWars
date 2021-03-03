5a315673229476e836cb26825fcf56197cf833ed
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-03-03 01:09:12 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [films, setFilms] = useState([]);
+  const [species, setSpecies] =  useState([]);
+  const [starships, setStarships] =  useState([]);
+  const [vehicles, setVehicles] =  useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchPeople(){
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.results);
+    }
+    async function fetchPlanets(){
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = await res.json();
+      setPlanets(data.results);
+    }
+    async function fetchFilms(){
+      let res = await fetch('https://swapi.dev/api/films/?format=json');
+      let data = await res.json();
+      setFilms(data.results);
+    }
+    async function fetchSpecies(){
+      let res = await fetch('https://swapi.dev/api/species/?format=json');
+      let data = await res.json();
+      setSpecies(data.results);
+    }
+    async function fetchStarships(){
+      let res = await fetch('https://swapi.dev/api/starships/?format=json');
+      let data = await res.json();
+      setStarships(data.results);
+    }
+    async function fetchVehicles(){
+      let res = await fetch('https://swapi.dev/api/vehicles/?format=json');
+      let data = await res.json();
+      setVehicles(data.results);
+    }
+    fetchPeople();
+    fetchPlanets();
+    fetchFilms();
+    fetchSpecies();
+    fetchStarships();
+    fetchVehicles();
+  }, [])
+  console.log("data", people);
+  console.log("data", planets);
+  /*console.log('films', films);
+  console.log('species', species);
+  console.log('starships', starships);
+  console.log('vehicles', vehicles);*/
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Star Wars
     </div>
   );
 }
